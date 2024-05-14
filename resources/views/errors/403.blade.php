bf74c8f5da935a9812d6ae4866aab8e83813d047
Rodrigo Alejandro Ramos Lozano <85802575+rodrigocoris@users.noreply.github.com>
2024-05-14 11:04:13 -0600

    error views fixed

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -4,46 +4,10 @@
 
 @section('content')
 
-<style>
-     body {
-        font-family: 'Turret Road', sans-serif; 
-        background-color: rgb(30, 31, 41);
-        margin: 0;
-        padding: 0;
-    }
-    .container-error-403 {
-        text-align: center;
-        padding: 1px;
-        margin-bottom: 300px; /* con esto cambio en espacio de las letras al footer */
-    }
-    .container-error-403 h1 {
-        font-size: 5em;
-        margin-top: 20vh;
-        color: white;
-        font-weight: bold; /* Hace el texto más grueso */
-    }
-    .container-error-403 h2 {
-        font-size: 3em;
-        color: white;
-    }
-    .container-error-403 a {
-        font-size: 1.2em;
-        color: white;
-        text-decoration: underline;
-    }
-    .container-error-403 .letters {
-        color: rgb(230, 17, 79);
-    }
-</style>
-
-<!--  fuente de Google Fonts -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Turret+Road&display=swap">
-
 <div class="container-error-403">
     <h1>Error - <span class="letters">403</span></h1>
     <h2><span class="letters">A</span>cceso <span class="letters">N</span>o <span class="letters">A</span>utorizado</h2>
-    <br>
-    <h6><a href="#">Volver a la página principal.</a></h6>
+    <h6><a href="inicio">Volver a la página principal.</a></h6>
 </div>
 
 @endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,66 +4,6 @@
 
 @section('content')
 
-<style>
-    body {
-      
-        background-color: rgb(30, 31, 41);
-        margin: 0;
-        padding: 0;
-    }
-    .container-error-404 {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 90vh;
-        position: relative;
-    }
-    .container-error-404 h1,
-    .container-error-404 h2,
-    .container-error-404 a {
-        margin: 10px 0;
-        text-align: center;
-        color: white;
-    }
-    .container-error-404 h1 {
-        font-size: 5vw; /* tamaño de la fuente a unidades relativas */
-        margin-top: 5vh;
-        color: white;
-        font-weight: bold; /* Hace el texto más grueso */
-    }
-    .container-error-404 h2 {
-        font-size: 2.5vw;
-        /*font-weight: bold; /* Hace el texto más grueso */
-    }
-    .container-error-404 a {
-        font-size: 1.2vw;
-        text-decoration: underline;
-    }
-    .container-error-404 .letters {
-        color: rgb(230, 17, 79);
-    }
-    /*efecto*/
-    .image-container-effect img {
-        max-width: 30%; /* Cambié el tamaño de la imagen a unidades relativas */
-        height: auto;
-        position: absolute;
-        top: 65%; /*  posición vertical de la imagen */
-        left: 70%; /* posición horizontal de la imagen */
-        transform: translate(-50%, -50%);
-    }
-    /*astro*/
-    .image-container-astro img {
-        max-width: 12%; /*tamaño de la imagen a unidades relativas */
-        height: auto;
-        position: absolute;
-        top: 68%; /*  posición vertical de la imagen */
-        left: 70%; /*  posición horizontal de la imagen */
-        transform: translate(-50%, -50%);
-    }
-</style>
-
-
 <div class="container-error-404">
     <h1>Error - <span class="letters">404</span></h1>
     <h2><span class="letters">P</span>AGINA <span class="letters">N</span>O <span class="letters">E</span>NCONTRADA</h2>
@@ -72,7 +12,6 @@
         <img src="images/error/effect.png" alt="">
     </div>
 </div>
-
 <div class="image-container-astro">
     <img src="images/error/astro.png" alt="">
 </div>
